c63f82303b8971731849167bec2c03c808c1519a
IliaValkov <ilia.valkov97@gmail.com>
2019-11-26 17:55:14 +0100

    refactored so that the script can be ran in any number of processes

--- script.py
+++ script.py
@@ -21,7 +21,6 @@ if rank == 0:
     for file in os.listdir(subsets_dir_path):
         file_path = os.path.join(subsets_dir_path, file)
         os.remove(file_path)
-    print(os.listdir(subsets_dir_path))
 
     # GET THE DATA
     train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
@@ -29,144 +28,154 @@ if rank == 0:
                                              origin=train_dataset_url)
     print("Local copy of the dataset file: {}".format(train_dataset_fp))
 
+    #
     with open(train_dataset_fp, 'r') as data_file:
         n_rows = sum(1 for row in data_file )
-        row_limit = math.ceil(n_rows/ (size-1))
+        row_limit = math.ceil(n_rows/ (size))
     
     csv_splitter.split(open(train_dataset_fp, 'r'), row_limit = row_limit, output_path = './subsets')
+
     subset_names = os.listdir(subsets_dir_path)
+    subset_names = [os.path.join(subsets_dir_path, name) for name in subset_names]
+    print(f"Subsets list : {subset_names}")
 
-    for i in range(len(subset_names)): 
-        path = "./subsets/"+subset_names[i]
-        comm.send(path, dest = (i+1))
-
-
-
-    while True: 
-        # TODO: Use gather 
-        g1 = comm.recv( source = 1)
-        g2 = comm.recv( source = 2)
-
-        g_reduced = []
-        
-        # TODO: maybe use zip, see how you'll implement the recieving
-        for i in range(len(g1)):
-            g_reduced.append( tf.math.add(g1[i] , g2[i]))
-
-        for g in g_reduced:
-            g = g / 2 # TODO: make dynamic 
-        
-        for i in range(2): # TODO: use broadcast; make dynamic
-            comm.send(g_reduced, dest = (i+1))
+    
+    train_subset_fp = subset_names
 
 else: 
+    row_limit = None
+    train_subset_fp = None
 
-    train_subset_fp = comm.recv(source = 0)
-    print(f"Process {rank} recieved name: {train_subset_fp}")
-    # PREPARE THE FEATURES AND LABELS NAMES
-    column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
-    features_names = column_names[:-1]
-    label_name = column_names[-1]
-    print(f"Features names: {features_names}")
-    print(f"Label: {label_name}")
+max_n_examples = comm.bcast(row_limit, root = 0 )
+train_subset_fp = comm.scatter(train_subset_fp, root = 0)
+
+print(f"Process {rank} recieved name: {train_subset_fp}")
+
+def getMaxNumberOfBatches (batch_size, maxNumberOfExamples) : 
+    return math.ceil(maxNumberOfExamples / batch_size)
+
+# PREPARE THE FEATURES AND LABELS NAMES
+column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+features_names = column_names[:-1]
+label_name = column_names[-1]
+print(f"Features names: {features_names}")
+print(f"Label: {label_name}")
 
 
-    # DECLARE CLASS NAMES
-    class_names = ["Iris_setosa", "Iris_versicolor", "Iris_virginica"]
+# DECLARE CLASS NAMES
+class_names = ["Iris_setosa", "Iris_versicolor", "Iris_virginica"]
 
-    # SPECIFY BATCH SIZE AND FORMAT THE DATA USING DATASET
-    batch_size = 10
-    train_dataset = tf.data.experimental.make_csv_dataset(
-      train_subset_fp,
-      batch_size,
-      column_names = column_names,
-      label_name = label_name,
-      num_epochs = 1)
+# SPECIFY BATCH SIZE AND FORMAT THE DATA USING DATASET
+batch_size = 32
+train_dataset = tf.data.experimental.make_csv_dataset(
+  train_subset_fp,
+  batch_size,
+  column_names = column_names,
+  label_name = label_name,
+  num_epochs = 1)
 
-    #print number of batches 
-    print(f"train_dataset: {train_dataset} rank: {rank}")
-   
-    def pack_features_vector(features, labels):
-        """Pack the features into a single array."""
-        features = tf.stack(list(features.values()), axis=1)
-        return features, labels
+# Get number of batches 
+max_n_batches = getMaxNumberOfBatches(batch_size , max_n_examples)
 
-    # CREATE SUITABLE FEATURES-LABEL PAIRS
-    train_dataset = train_dataset.map(pack_features_vector)
+def pack_features_vector(features, labels):
+    """Pack the features into a single array."""
+    features = tf.stack(list(features.values()), axis=1)
+    return features, labels
 
-    # DECLARE THE MODEL
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
-        tf.keras.layers.Dense(10, activation=tf.nn.relu),
-        tf.keras.layers.Dense(3)
-    ])
+# CREATE SUITABLE FEATURES-LABEL PAIRS
+train_dataset = train_dataset.map(pack_features_vector)
 
-    # DECALARE A LOSS FUNCTION
-    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+# DECLARE THE MODEL
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
+    tf.keras.layers.Dense(10, activation=tf.nn.relu),
+    tf.keras.layers.Dense(3)
+])
 
-    # GET A BATCH OF FEATURES AND LABELS
-    features, labels = next(iter(train_dataset))
+# DECALARE A LOSS FUNCTION
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-    # FUNCTION TO CALCULATE THE LOSS
-    def loss(model, x, y):
-      y_ = model(x)
+# GET A BATCH OF FEATURES AND LABELS
+features, labels = next(iter(train_dataset))
 
-      return loss_object(y_true=y, y_pred=y_)
+# FUNCTION TO CALCULATE THE LOSS
+def loss(model, x, y):
+  y_ = model(x)
 
-    # CALCULATE LOSS PRE-TRAINING
-    l = loss(model, features, labels)
+  return loss_object(y_true=y, y_pred=y_)
 
-    # FUNCTION TO CALCULATE THE GRADIENTS
-    def grad(model, inputs, targets):
-      with tf.GradientTape() as tape:
-        loss_value = loss(model, inputs, targets)
-      return loss_value, tape.gradient(loss_value, model.trainable_variables)
+# CALCULATE LOSS PRE-TRAINING
+l = loss(model, features, labels)
 
-    # APPLIES THE COMPUTED GRADIENTS TO THE MODEL'S VARIABLES TO MINIMIZE THE LOSS FUNCTION
-    optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+# FUNCTION TO CALCULATE THE GRADIENTS
+def grad(model, inputs, targets):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets)
+  return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
-    # Keep results for plotting
-    train_loss_results = []
-    train_accuracy_results = []
+# APPLIES THE COMPUTED GRADIENTS TO THE MODEL'S VARIABLES TO MINIMIZE THE LOSS FUNCTION
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
 
-    num_epochs = 201
+# Keep results for plotting
+train_loss_results = []
+train_accuracy_results = []
 
-    print("Loss test: {}".format(l))
+num_epochs = 201
+print("Loss test: {}".format(l))
+print("max_n_batches {}".format(max_n_batches) )
 
-    # EPOCH LOOP
-    for epoch in range(num_epochs):
-      
-        # COMPUTES THE (WEIGHTED) MEAN OF THE GIVEN VALUES
-        epoch_loss_avg = tf.keras.metrics.Mean()
 
-        # CALCULATES HOW OFTEN PREDICTIONS MATCHES INTEGER LABELS
-        epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+# EPOCH LOOP
+for epoch in range(num_epochs):
+  
+    # COMPUTES THE (WEIGHTED) MEAN OF THE GIVEN VALUES
+    epoch_loss_avg = tf.keras.metrics.Mean()
 
-        # TRAINING LOOP - using batches of 32
-        for x, y in train_dataset:
-        # Optimize the model
+    # CALCULATES HOW OFTEN PREDICTIONS MATCHES INTEGER LABELS
+    epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+    
+    set_iter = iter(train_dataset)
 
+    # TRAINING LOOP - using batches of 32
+    for i in range(max_n_batches):
+        try: 
+            x, y = next(set_iter)
             # Compute loss value and gradients
             loss_value, grads = grad(model, x, y)
+            
+            # send to master node for reducing
+            payload = grads
+        
+        except StopIteration as e: 
+            # signalize you dont have anything to send 
+            payload = "EOS"
+        
+        # Gather a list of all gradients or "EOS" messages
+        #print(f"Sending {type(payload)} from rank {rank} i : {i}")
+        payload_gather = comm.gather(payload, root = 0) 
+        if rank == 0 : 
+            filtered = filter(lambda n: n != "EOS", payload_gather)
 
-            # apply gradients to model
-            # print(f"Sending grads from rank: {rank}")
-            comm.send(grads, dest = 0)
+            sumed = None 
+            reduced = []
+            for grad_tuple in zip(*filtered): 
+                reduced.append(tf.math.add_n (list(grad_tuple))) 
+        else: 
+            reduced = 0
 
-            g = comm.recv(source = 0)
-            #print(f"recieved g: {g}" )
-            optimizer.apply_gradients(zip(g, model.trainable_variables))
+        recieved_gradients = comm.bcast(reduced, root = 0)
+        #print(f"recieved g: {g}" )
+        optimizer.apply_gradients(zip(recieved_gradients, model.trainable_variables))
 
-            # Track progress
-            epoch_loss_avg(loss_value)  # Add current batch loss
-            epoch_accuracy(y, model(x)) # Compare predicted label to actual label
+        # Track progress
+        epoch_loss_avg(loss_value)  # Add current batch loss
+        epoch_accuracy(y, model(x)) # Compare predicted label to actual label
 
 
-        # End epoch
-        train_loss_results.append(epoch_loss_avg.result())
-        train_accuracy_results.append(epoch_accuracy.result())
+    # End epoch
+    train_loss_results.append(epoch_loss_avg.result())
+    train_accuracy_results.append(epoch_accuracy.result())
 
-        if epoch % 50 == 0:
-            print("Process: {:01d}  Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(rank,epoch,epoch_loss_avg.result(),epoch_accuracy.result()))
-    
-    comm.send("Exit", dest = 0)
+    if epoch % 50 == 0:
+        print("Process: {:01d}  Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(rank,epoch,epoch_loss_avg.result(),epoch_accuracy.result()))
+
