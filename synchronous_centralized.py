da3607ea45d6c2c59ccc67bf9de565a2e5419ee2
IliaValkov <ilia.valkov97@gmail.com>
2019-12-16 16:25:04 +0100

    device placement .03

--- synchronous_centralized.py
+++ synchronous_centralized.py
@@ -1,5 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals 
 import tensorflow as tf 
+from mpi4py import MPI 
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
 gpus = tf.config.experimental.list_physical_devices('GPU')
 
 device_name = gpus[rank].name 
@@ -12,14 +18,10 @@ with tf.device(device_name):
     import math
     import os
     import time
-    from mpi4py import MPI 
 
     tf.debugging.set_log_device_placement(True)
 
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
 
     print(f"Tensorflow vesion: {tf.__version__}") 
     print(f"Eager execition: {tf.executing_eagerly()}")
