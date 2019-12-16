ba57313e9978267cdc153def3a9c1c3806d03236
IliaValkov <ilia.valkov97@gmail.com>
2019-12-16 16:13:43 +0100

    removed module from testpy

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals 
-import matplotlib.pyplot as plt 
+
 import tensorflow as tf 
 from tools import csv_splitter
 import math
