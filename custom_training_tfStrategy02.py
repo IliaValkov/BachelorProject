f25727244bbac38a128c894a8f9c1ce545481958
IliaValkov <ilia.valkov97@gmail.com>
2019-12-27 15:59:00 +0100

    added print statement

--- custom_training_tfStrategy02.py
+++ custom_training_tfStrategy02.py
@@ -34,6 +34,7 @@ class_names = ["Iris_setosa", "Iris_versicolor", "Iris_virginica"]
 
 # SPECIFY BATCH SIZE AND FORMAT THE DATA USING DATASET
 batch_size = b_size * strategy.num_replicas_in_sync
+print(f"batch_size: {batch_size}")
 train_dataset = tf.data.experimental.make_csv_dataset(
     train_dataset_fp,
     batch_size,
